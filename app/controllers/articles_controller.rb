bb11465e390398126e7b9e9e9316527d71494459
tarek213 <tarek.mohamedcsed21@gmail.com>
2019-02-04 23:36:59 +0200

    help

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,21 @@ def new
 end
 
 def create
-render plain: params[:articles].inspect
+
+@article = Article.new(article_params)
+
+@article.save
+
+redirect_to article_path(@article)
+
 end
 
+private
+
+def article_params
+
+params.require(:article).permit(:title, :description)
+
+end
 
 end
