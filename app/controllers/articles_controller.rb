b32f5f870ba613a67b74553c905a31f4f16e75c1
tarek213 <tarek.mohamedcsed21@gmail.com>
2019-02-05 02:44:13 +0200

    database with show completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,20 @@ end
 def create
 
 @article = Article.new(article_params)
-
-@article.save
-
+#
+# @article.save
+#
+# redirect_to article_path(@article)
+if @article.save
+flash[:notice] = "Article was successfully created"
 redirect_to article_path(@article)
+else
+render 'new'
+end
+
+def show
+@article= Article.find(params[:id])
+end
 
 end
 
