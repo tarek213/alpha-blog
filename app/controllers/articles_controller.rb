ce012cebeb0bb34074c5a2fa49072619ac6f030a
tarek213 <tarek.mohamedcsed21@gmail.com>
2019-02-05 21:25:44 +0200

    edit and show links with back to origin page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+def index
+@articles =Article.all
+end
 
 def new
 @article = Article.new
@@ -18,11 +21,31 @@ else
 render 'new'
 end
 
+end
+
 def show
 @article= Article.find(params[:id])
 end
 
+
+def edit
+
+@article = Article.find(params[:id])
+
 end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "Article has been updated"
+    redirect_to article_path( @article )
+  else
+    render 'edit'
+  end
+
+end
+
+
 
 private
 
